abf0c6ecc78c6e945aab3dfa0146022ed5533e38
sandrovisky <sndr95@hotmail.com>
2021-01-14 19:20:35 -0300

    wesley e um bost a

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+//wesley e um bosta
 function imprimir(){
     
     var texto = document.getElementById("texto").value
