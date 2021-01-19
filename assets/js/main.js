43d33b5f254317e76d19e93071e4b32e74db8725
sandrovisky <sndr95@hotmail.com>
2021-01-19 15:45:41 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,9 @@ function imprimir(){
 
     var printWindow = window.open("", "Print Window","height=600,width=600");
     printWindow.document.write("<html><head>");
-    printWindow.document.write("</head><body ><p><h1 style='text-align: center;' >Impressão do Sandrovisky</h1><br/><br/><br/><br/><br/><p style='text-align: center;'></p>");
-    printWindow.document.write(`<img  style = 'display: block;margin-left: auto;margin-right: auto;width: 50%;border-style: solid; border-width: 5px;' src='https://www.cognex.com/api/Sitecore/Barcode/Get?data=${texto}&code=BCL_CODE128&width=800&imageType=PNG&foreColor=%23000000&backColor=%23FFFFFF&rotation=RotateNoneFlipNone'  width='1000' />`);
+    ///printWindow.document.write("<style>@media print and (width: 10cm) and (height: 10cm) { @page { margin: 3cm;}}</style>");
+    printWindow.document.write("</head><body style = 'width: 10cm;height:5cm;border-style: solid; border-width: 1px;'><h1 style='text-align: center;' >Impressão do Sandrovisky</h1><br/>");
+    printWindow.document.write(`<img  style = 'display: block;margin-left: auto;margin-right: auto;width: 50%;' src='https://www.cognex.com/api/Sitecore/Barcode/Get?data=${texto}&code=BCL_CODE128&width=800&imageType=PNG&foreColor=%23000000&backColor=%23FFFFFF&rotation=RotateNoneFlipNone'  width='1000' />`);
     printWindow.document.write("</body></html>");
     printWindow.document.close();
     setTimeout(function(){printWindow.print();  }, 500);    
